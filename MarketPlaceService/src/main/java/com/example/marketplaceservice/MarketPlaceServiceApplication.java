25534211d8bcf11b79989d0877e95e2e7f09c91f
Hamza Iddaoui <hamzaiddaoui99@gmail.com>
2023-12-06 15:00:55 +0100

    Project Structure

---
+++ MarketPlaceService/src/main/java/com/example/marketplaceservice/MarketPlaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketplaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketPlaceServiceApplication.class, args);
+    }
+
+}
