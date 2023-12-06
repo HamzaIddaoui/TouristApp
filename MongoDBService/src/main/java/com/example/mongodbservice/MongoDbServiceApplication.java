c2395d6fc836551c33c3b0931422be10d7284ae0
ikramEssakhi <ikram.ess00@gmail.com>
2023-12-06 14:29:44 +0100

    hah

--- MongoDBService/src/main/java/com/example/mongodbservice/MongoDbServiceApplication.java
+++ MongoDBService/src/main/java/com/example/mongodbservice/MongoDbServiceApplication.java
@@ -5,12 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongoDbServiceApplication {
-<<<<<<< HEAD
-    System.out.println("Hey");
-=======
 
     System.out.println("hhhhhhhhhhhh");
->>>>>>> master
     public static void main(String[] args) {
         SpringApplication.run(MongoDbServiceApplication.class, args);
     }
