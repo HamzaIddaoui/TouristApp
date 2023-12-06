d96759b07d6d2bbf6493e0aa5711761c946e5c0e
Hamza Iddaoui <hamzaiddaoui99@gmail.com>
2023-12-06 14:18:22 +0100

    Test commit

--- MongoDBService/src/main/java/com/example/mongodbservice/MongoDbServiceApplication.java
+++ MongoDBService/src/main/java/com/example/mongodbservice/MongoDbServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongoDbServiceApplication {
-
+    System.out.println("Hey");
     public static void main(String[] args) {
         SpringApplication.run(MongoDbServiceApplication.class, args);
     }
